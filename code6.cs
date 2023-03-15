3fb19ba1be858663122d0aa5a33ffd0aa1c8bfc3
Andressa Abrantes <andressaabrantes@gmail.com>
2023-03-15 15:37:46 -0300

    FreeCodeCamp: Getting user inputs

---
+++ code6.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + ", you're " + age + " years old!");
+        }
+    }
+}
