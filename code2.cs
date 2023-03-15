9dcce247be32e6f2aa28bb912edbd8e6aa3c6f29
Andressa Abrantes <andressaabrantes@gmail.com>
2023-03-15 10:17:58 -0300

    FreeCodeCamp: Variables

---
+++ code2.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Tom";
+            int characterAge;
+            characterAge = 25;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+
+            characterAge = 28;
+            Console.WriteLine("But didn't like being " + characterAge);
+        }
+    }
+}
