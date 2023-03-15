6bfa703b0b769fcd794b65f79bd7c1315c19e7d0
Andressa Abrantes <andressaabrantes@gmail.com>
2023-03-15 12:02:48 -0300

    FreeCodeCamp: Data Types

---
+++ code3.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+            char grade = 'A'; //only one character
+            int age = 30; //only positive numbers
+            double gpa = 3.3;
+            bool isFemale = true; //either should be true or false
+
+            Console.WriteLine(30); 
+        }
+    }
+}
